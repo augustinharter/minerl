5396bf760f9a41b20d48cadc7d60d0935c8789c1
Augustin Harter <aharter@techfak.uni-bielefeld.de>
2020-12-31 16:44:55 +0100

    add hue to run_embed_setups

--- run_embed_setups.sh
+++ run_embed_setups.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-for delay in  0 10; do
-    for dim in 32 64 100 ; do
-        for clusters in 32 64 100 ; do
+for delay in 0 10; do
+    for dim in 64 100 ; do
+        for clusters in 64 100 ; do
             for threshold in 0.8 0.9  ; do
                 for patchwid in 6 8 10 ; do
                     python3 TrainHandler.py --datasize 10000 --embed-train-samples 1000 --embed-dim $dim \
-                    --embed-cluster $clusters --embed-pos-threshold $threshold --delay $delay --embed-patch-width $patchwid
+                    --embed-cluster $clusters --embed-pos-threshold $threshold --delay $delay --embed-patch-width $patchwid -hue
                 done
             done
         done
